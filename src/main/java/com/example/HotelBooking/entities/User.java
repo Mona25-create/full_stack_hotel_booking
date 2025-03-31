1e8a96f2b27a90cf02cf0e00645660954131ecf2
Mona25-create <monagupta2570@gmail.com>
2025-03-31 12:26:55 +0530

    initial commit

---
+++ src/main/java/com/example/HotelBooking/entities/User.java
@@ -0,0 +1,47 @@
+package com.example.HotelBooking.entities;
+
+import java.time.LocalDateTime;
+
+import com.example.HotelBooking.enums.UserRole;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "users") //wil be created in db
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Email is required")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "Password number is required")
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    @NotBlank(message = "Phone Number number is required")
+    private String phoneNumber;
+    @Enumerated(EnumType.STRING)
+    private UserRole role; //eg customer or admin
+
+    private boolean isActive;
+    private final LocalDateTime createAt = LocalDateTime.now();
+    
+}
